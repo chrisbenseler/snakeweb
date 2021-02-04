e9164697cc3709a0f638f294c8a9cbfbb4a26d62
Chris Benseler <cbenseler@gmail.com>
2021-02-04 13:49:34 -0300

    feat: snake eat food

--- src/board.js
+++ src/board.js
@@ -3,6 +3,8 @@ import { Snake } from "./snake.js";
 const Board = ({ size = 16 }) => {
   const _board = {};
 
+  let currentFood = null;
+
   Array(size)
     .fill()
     .forEach((_, indexX) => {
@@ -23,6 +25,27 @@ const Board = ({ size = 16 }) => {
     Object.keys(_board).forEach((key) => {
       _board[key] = { ..._board[key], snake: snakeCoords.indexOf(key) >= 0 };
     });
+
+    const head = snake.head();
+    if((head.x + "_" + head.y) === currentFood) {
+      snake.grow();
+      currentFood = null;
+      _board["12_5"].food = false;
+    }
+  };
+
+  const addFood = () => {
+    const snakeCoords = [snake.head(), ...snake.tail()].map(
+      (c) => c.x + "_" + c.y
+    );
+
+    const emptyCells = Object.keys(_board).filter(
+      (key) => snakeCoords.indexOf(key) < 0
+    );
+
+    //_board[emptyCells[0]].food = true;
+    _board["12_5"].food = true;
+    currentFood = "12_5";
   };
 
   update();
@@ -33,6 +56,7 @@ const Board = ({ size = 16 }) => {
     status: () => {
       return { ..._board };
     },
+    addFood,
     tick: () => {
       snake.move();
       update();
